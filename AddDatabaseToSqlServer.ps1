58344a94a0d9118f355b10a9658c9b323138c3df
Paul Hernandez <paul.pauldj54@gmail.com>
2020-10-02 20:28:02 +0200

    Update AddDatabaseToSqlServer.ps1
    Configuring TLS1.2 or higher on the system

--- AddDatabaseToSqlServer.ps1
+++ AddDatabaseToSqlServer.ps1
@@ -6,6 +6,12 @@ param(
     $password
 )
 
+# Set strong cryptography on 64 bit .Net Framework (version 4 and above)
+Set-ItemProperty -Path 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\.NetFramework\v4.0.30319' -Name 'SchUseStrongCrypto' -Value '1' -Type DWord
+
+# set strong cryptography on 32 bit .Net Framework (version 4 and above).
+Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\.NetFramework\v4.0.30319' -Name 'SchUseStrongCrypto' -Value '1' -Type DWord
+
 if ((Get-Command Install-PackageProvider -ErrorAction Ignore) -eq $null)
 {
 	# Load the latest SQL PowerShell Provider
